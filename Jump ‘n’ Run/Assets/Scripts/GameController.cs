ca49abbf98669e05d2b4bac55137c94d6143f24a
wenlop <wendy.alq@gmail.com>
2018-06-17 21:41:53 +0200

    Einfaches 2D Jump ‘n’ Run

---
+++ Jump ‘n’ Run/Assets/Scripts/GameController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+//enumerator
+public enum GameState { Idle, Playing, Ended, Ready};
+
+public class GameController : MonoBehaviour {
+    
+	//Min and max bar
+	[Range(0f, 0.20f)]
+	public float ParallaxSpeed = 0.02f;
+	public RawImage background;
+	public RawImage platform;
+	public GameObject UI_Idle;
+	public GameObject UI_Score;
+	public Text pointsText;
+	public GameState gameState = GameState.Idle; //Idle
+
+	public GameObject player;
+	public GameObject enemyGenerator;
+
+	private int points = 0;
+   
+
+	// Use this for initialization
+	void Start () { 
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//Spiel starten
+		if (gameState == GameState.Idle && (Input.GetKeyDown("up") || Input.GetMouseButtonDown(0))){ //Pfeil nach oben oder Linke Maustaste 
+			gameState = GameState.Playing;
+			UI_Idle.SetActive(false); //nicht mehr aktiv
+			UI_Score.SetActive(true);
+			player.SendMessage("UpdateState", "PlayerRun");
+			enemyGenerator.SendMessage("StartGenerator");
+		}
+		//Spiel läuft
+		else if (gameState == GameState.Playing){
+			Parallax();
+		}
+		//Spiel beendet und bereit zum Neustart
+		else if (gameState == GameState.Ready){
+			//RestartGame
+			if (Input.GetKeyDown("up") || Input.GetMouseButtonDown(0)){
+				RestartGame();
+			}
+        }
+	}
+	void Parallax(){
+		float finalSpeed = ParallaxSpeed * Time.deltaTime;
+        background.uvRect = new Rect(background.uvRect.x + finalSpeed, 0f, 1f, 1f);
+        platform.uvRect = new Rect(platform.uvRect.x + finalSpeed * 4, 0f, 1f, 1f);
+	}
+
+	public void RestartGame(){
+		SceneManager.LoadScene("Jump");
+	}
+
+	public void IncreasePoints(){
+		points++;
+		pointsText.text = points.ToString();
+	}
+}
---
+++ Jump ‘n’ Run/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public GameObject game;
+	public GameObject enemyGenerator;
+
+	private Animator animator;
+
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Überprüfung des aktuellen Status - playing
+		bool gamePlaying = game.GetComponent<GameController>().gameState == GameState.Playing;
+		if (gamePlaying && (Input.GetKeyDown("up") || Input.GetMouseButtonDown(0))){
+			UpdateState("PlayerJump");
+		}
+		
+	}
+
+	public void UpdateState(string state = null){
+		if (state != null){
+			animator.Play(state);
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+        if (other.gameObject.tag == "Enemy"){
+			//Debug.Log("I'm dying!!!!");
+			UpdateState("PlayerDie");
+			game.GetComponent<GameController>().gameState = GameState.Ended;
+			enemyGenerator.SendMessage("CancelGenerator", true); // keine Feinde mehr
+			//Koalition - Punkte
+		}else if (other.gameObject.tag == "Point"){
+			game.SendMessage("IncreasePoints");
+		}
+    }
+
+	void GameReady(){
+		game.GetComponent<GameController>().gameState = GameState.Ready;
+	}
+}
