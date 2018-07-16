5a2328471d1037756052bf0791f5d6107fd445dc
wenlop <wendy.alq@gmail.com>
2018-07-16 22:25:37 +0200

    Third  Unity Project

---
+++ 3_UnityProject_Pingpong/Assets/Script/Ball.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Ball : MonoBehaviour {
+
+	int speedX;
+	int speedY;
+
+	float speed;
+
+	//Score
+
+	public Text scoreText;
+
+	int player1Score;
+	int player2Score;
+
+	//winner
+	public Text winner;
+
+	public ParticleSystem sparks;
+
+	// Use this for initialization
+	void Start () {
+
+		MoveBall ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		scoreText.text = player1Score.ToString() + " - " + player2Score.ToString();
+
+		if (player1Score == 2){
+			winner.text = "PLAYER 1 WIN";
+			winner.gameObject.SetActive(true);
+			ResetBall();
+		}
+
+		if (player2Score == 2)
+        {
+			winner.text = "PLAYER 2 WIN";
+			winner.gameObject.SetActive(true);
+			ResetBall();
+        }
+		
+	}
+
+	void MoveBall(){
+		//scoreText.text = "test";
+
+        speed = Random.Range(5, 10);
+
+        // richtung
+        speedX = Random.Range(0, 2);
+        if(speedX == 0){
+            speedX = 1;
+        }else{
+            speedX = -1;
+        }
+
+        speedY = Random.Range(0, 2);
+        if (speedX == 0)
+        {
+            speedY = 1;
+        }
+        else
+        {
+            speedY = -1;
+        }
+
+        GetComponent<Rigidbody>().velocity = new Vector3(speed* speedX, speed* speedY, 0); // velocidad de la bola - velocidad*direccion
+        
+	}
+
+	// Damit jedes Mal, wenn der Spieler einen Punk macht, wieder von vorne anfängt.
+	void ResetBall(){
+		transform.localPosition = new Vector3(0, 0, 0);
+		GetComponent<Rigidbody>().velocity = Vector3.zero;
+	}
+
+
+	// Punktesumme der beiden Spieler
+	void OnCollisionEnter(Collision goal){
+
+		if (goal.collider.tag=="player1goal"){
+			player1Score++;
+			ResetBall ();
+			Invoke("MoveBall",2); //Um der Methode mitzuteilen, wann und zu welcher Zeit sie aufgerufen werden soll
+		}
+
+		if (goal.collider.tag == "player2goal"){
+            player2Score++;
+			ResetBall ();
+			Invoke("MoveBall", 2);
+        }
+
+		if (goal.collider.tag == "Player"){
+			sparks.Play();
+        }
+	}
+
+}
---
+++ 3_UnityProject_Pingpong/Assets/Script/Player.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public KeyCode up;
+	public KeyCode down;
+
+	float speed;
+	float finalspeed;
+
+	// Use this for initialization
+	void Start () {
+		// Geschwindigkeit, mit der wir die Schaufel des Spielers bewegen.
+		speed = 0.15f;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (Input.GetKey(up)){// Wenn Sie die Aufwärtstaste drücken, sollten Sie das Paddel nach oben bewegen.
+
+			if (transform.localPosition.y > 4 ){
+				finalspeed = 0;
+			} else {
+				finalspeed = speed;
+			}
+		transform.Translate(0,finalspeed,0);	
+		} 
+
+		if (Input.GetKey(down)){ 
+		
+		    if (transform.localPosition.y < -4 ){
+                finalspeed = 0;
+            }else{
+                finalspeed = speed;
+            }
+            transform.Translate(0,-finalspeed,0); 
+        } 
+        
+	}
+}
